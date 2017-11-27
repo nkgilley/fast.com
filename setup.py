cbafc132b967f35a01d71ef166577e63ef3b79fe
Nolan Gilley <nkgilley@gmail.com>
2017-11-27 09:01:40 -0500

    0.0.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ SOFTWARE.
 """
 
 setup(name='fastdotcom',
-      version='0.0.2',
+      version='0.0.3',
       description='Python API for testing internet speed on Fast.com',
       url='https://github.com/nkgilley/fast.com',
       author='Nolan Gilley',
